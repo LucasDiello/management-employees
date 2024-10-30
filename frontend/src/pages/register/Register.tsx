1408982d3c5bfb9fa8d310836a80010658f37654
LucasDiello <lucasoliveiradiello@hotmail.com>
2024-10-29 23:28:37 -0300

    feat: increment register page

---
+++ frontend/src/pages/register/Register.tsx
@@ -0,0 +1,244 @@
+import * as React from "react";
+import { CssVarsProvider, extendTheme, useColorScheme } from "@mui/joy/styles";
+import GlobalStyles from "@mui/joy/GlobalStyles";
+import CssBaseline from "@mui/joy/CssBaseline";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Checkbox from "@mui/joy/Checkbox";
+import Divider from "@mui/joy/Divider";
+import FormControl from "@mui/joy/FormControl";
+import FormLabel from "@mui/joy/FormLabel";
+import IconButton, { IconButtonProps } from "@mui/joy/IconButton";
+import Link from "@mui/joy/Link";
+import Input from "@mui/joy/Input";
+import Typography from "@mui/joy/Typography";
+import Stack from "@mui/joy/Stack";
+import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
+import LightModeRoundedIcon from "@mui/icons-material/LightModeRounded";
+import BadgeRoundedIcon from "@mui/icons-material/BadgeRounded";
+import GoogleIcon from "../login/GoogleIcon";
+
+interface FormElements extends HTMLFormControlsCollection {
+  fullName: HTMLInputElement;
+  email: HTMLInputElement;
+  password: HTMLInputElement;
+  confirmPassword: HTMLInputElement;
+  terms: HTMLInputElement;
+}
+interface SignUpFormElement extends HTMLFormElement {
+  readonly elements: FormElements;
+}
+
+function ColorSchemeToggle(props: IconButtonProps) {
+  const { onClick, ...other } = props;
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => setMounted(true), []);
+
+  return (
+    <IconButton
+      aria-label="alternar modo claro/escuro"
+      size="sm"
+      variant="outlined"
+      disabled={!mounted}
+      onClick={(event) => {
+        setMode(mode === "light" ? "dark" : "light");
+        onClick?.(event);
+      }}
+      {...other}
+    >
+      {mode === "light" ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+    </IconButton>
+  );
+}
+
+const customTheme = extendTheme({ colorSchemes: { light: {}, dark: {} } });
+
+export default function Register() {
+  return (
+    <CssVarsProvider theme={customTheme} disableTransitionOnChange>
+      <CssBaseline />
+      <GlobalStyles
+        styles={{
+          ":root": {
+            "--Form-maxWidth": "800px",
+            "--Transition-duration": "0.4s",
+          },
+        }}
+      />
+      <Box
+        sx={(theme) => ({
+          height: "100%",
+          position: "fixed",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          width: { xs: "100%", md: "48vw" },
+          transition:
+            "background-image var(--Transition-duration), left var(--Transition-duration) !important",
+          transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
+          backgroundColor: "background.level1",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1527181152855-fc03fc7949c8?auto=format&w=1000&dpr=2)",
+          [theme.getColorSchemeSelector("dark")]: {
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1572072393749-3ca9c8ea0831?auto=format&w=1000&dpr=2)",
+          },
+        })}
+      />
+      <Box
+        sx={(theme) => ({
+          width: { xs: "100%", md: "50vw" },
+          ml: { xs: 0, md: "48vw" },
+          transition: "width var(--Transition-duration)",
+          transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
+          position: "relative",
+          zIndex: 1,
+          display: "flex",
+          justifyContent: "flex-end",
+          backdropFilter: "blur(12px)",
+          backgroundColor: "rgba(255 255 255 / 0.2)",
+          [theme.getColorSchemeSelector("dark")]: {
+            backgroundColor: "rgba(19 19 24 / 0.4)",
+          },
+        })}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "100dvh",
+            width: "100%",
+            px: 2,
+          }}
+        >
+          <Box
+            component="header"
+            sx={{ py: 3, display: "flex", justifyContent: "space-between" }}
+          >
+            <Box sx={{ gap: 2, display: "flex", alignItems: "center" }}>
+              <IconButton variant="soft" color="primary" size="sm">
+                <BadgeRoundedIcon />
+              </IconButton>
+              <Typography level="title-lg">Taugor</Typography>
+            </Box>
+            <ColorSchemeToggle />
+          </Box>
+          <Box
+            component="main"
+            sx={{
+              my: "auto",
+              py: 2,
+              pb: 5,
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+              width: 400,
+              maxWidth: "100%",
+              mx: "auto",
+              borderRadius: "sm",
+              "& form": {
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+              },
+              [`& .MuiFormLabel-asterisk`]: {
+                visibility: "hidden",
+              },
+            }}
+          >
+            <Stack sx={{ gap: 4, mb: 2 }}>
+              <Stack sx={{ gap: 1 }}>
+                <Typography component="h1" level="h3">
+                  Cadastre-se
+                </Typography>
+                <Typography level="body-sm">
+                  Já tem uma conta?{" "}
+                  <Link href="/login" level="title-sm">
+                    Faça login!
+                  </Link>
+                </Typography>
+              </Stack>
+              <Button
+                variant="soft"
+                color="neutral"
+                fullWidth
+                startDecorator={<GoogleIcon />}
+              >
+                Continuar com o Google
+              </Button>
+            </Stack>
+            <Divider
+              sx={(theme) => ({
+                [theme.getColorSchemeSelector("light")]: {
+                  color: { xs: "#FFF", md: "text.tertiary" },
+                },
+              })}
+            >
+              ou
+            </Divider>
+            <Stack sx={{ gap: 4, mt: 2 }}>
+              <form
+                onSubmit={(event: React.FormEvent<SignUpFormElement>) => {
+                  event.preventDefault();
+                  const formElements = event.currentTarget.elements;
+                  const data = {
+                    fullName: formElements.fullName.value,
+                    email: formElements.email.value,
+                    password: formElements.password.value,
+                    confirmPassword: formElements.confirmPassword.value,
+                    terms: formElements.terms.checked,
+                  };
+                  alert(JSON.stringify(data, null, 2));
+                }}
+              >
+                <FormControl required>
+                  <FormLabel>Nome Completo</FormLabel>
+                  <Input type="text" name="fullName" />
+                </FormControl>
+                <FormControl required>
+                  <FormLabel>Email</FormLabel>
+                  <Input type="email" name="email" />
+                </FormControl>
+                <FormControl required>
+                  <FormLabel>Senha</FormLabel>
+                  <Input type="password" name="password" />
+                </FormControl>
+                <FormControl required>
+                  <FormLabel>Confirmar Senha</FormLabel>
+                  <Input type="password" name="confirmPassword" />
+                </FormControl>
+                <Stack sx={{ gap: 4, mt: 2 }}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Checkbox size="sm" label="Aceito os termos" name="terms" />
+                    <Link level="title-sm" href="#replace-with-a-link">
+                      Termos e Condições
+                    </Link>
+                  </Box>
+                  <Button type="submit" fullWidth>
+                    Cadastre-se
+                  </Button>
+                </Stack>
+              </form>
+            </Stack>
+          </Box>
+          <Box component="footer" sx={{ py: 3 }}>
+            <Typography level="body-xs" sx={{ textAlign: "center" }}>
+              © Taugor {new Date().getFullYear()}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </CssVarsProvider>
+  );
+}
